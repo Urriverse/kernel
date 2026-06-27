ea6e96c69bd5dbc57d3eca9baa9b55c484f75e2c
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-27 08:17:33 +0300

    Preparing for KMI

--- src/kmi/mbs.rs
+++ src/kmi/mbs.rs
@@ -4,7 +4,19 @@ use alloc::sync::Arc;
 
 use crate::sched::proc::Process;
 
-pub fn run_module(_elf: &[u8]) -> Result<Arc<Process>, usize> {
+pub fn run_module(elf: &[u8]) -> Result<Arc<Process>, usize> {
     debug!("");
+
+    let bytes = elf::ElfBytes::<elf::endian::NativeEndian>::minimal_parse(elf).expect("Can't parse bootstrap module");
+
+    for s in bytes.segments().expect("X").iter() {
+        debug!("S {:?}", s);
+    }
+    debug!("");
+
+    for s in bytes.symbol_table().expect("Y").iter() {
+        debug!("G {:?}", s);
+    }
+
     Err(!0)
 }
