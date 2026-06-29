b2bab030fcdb9120f04f47aefb9d95661f1fc9c4
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-29 07:18:55 +0300

    Preparing for KMI

--- src/kmi/front.rs
+++ src/kmi/front.rs
@@ -1,12 +1,12 @@
 #![allow(non_snake_case)]
 
-use ketypes::*;
-use nopaque::*;
+// use ketypes::*;
+// use nopaque::*;
 
-pub extern "Rust" fn KeVtDeviceNew(name: KeStr) -> Arc![@Device] {
-    trace!("KeVtDeviceNew name={:?}", name);
-    <arc![@Device]>::new(crate::dev::Device::new(name))
-}
+// pub extern "Rust" fn KeVtDeviceNew(name: KeStr) -> Arc![@Device] {
+//     trace!("KeVtDeviceNew name={:?}", name);
+//     <arc![@Device]>::new(crate::dev::Device::new(name))
+// }
 
 pub fn KeTest() {
     error!("TEST");
