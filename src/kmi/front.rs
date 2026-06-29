90fb21fb31eb49d020177f63d71e11f74d6df4f3
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-29 06:55:23 +0300

    Preparing for KMI

--- src/kmi/front.rs
+++ src/kmi/front.rs
@@ -7,3 +7,7 @@ pub extern "Rust" fn KeVtDeviceNew(name: KeStr) -> Arc![@Device] {
     trace!("KeVtDeviceNew name={:?}", name);
     <arc![@Device]>::new(crate::dev::Device::new(name))
 }
+
+pub extern "Rust" fn KeTest() {
+    error!("TEST");
+}
