b294c01952791332b45abc4ae3922a543684cb74
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-30 20:24:59 +0300

    Preparing for KMI

--- src/kmi/mod.rs
+++ src/kmi/mod.rs
@@ -97,7 +97,7 @@ pub fn init(elf: &[u8]) {
     // link it to the kernel
     for sym in symtab {
         if let Ok(name) = strtab.get(sym.st_name as usize) {
-            trace!("Found symbol `{}`", name);
+            // trace!("Found symbol `{}`", name);
             if KESYMTAB.contains_key(&name) {
                 trace!("Linking `{}` -> {:p}...", name, KESYMTAB[&name] as *const ());
                 if let Some(r) = module.dive(&sym) {
